d8eb53181f79a88f212a847438e9351fd6763600
hicass <hicass.dev@gmail.com>
2025-01-29 15:34:12 -0800

    Add a simple dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const DashboardPage: FC = () => {
+  return (
+    <main className='border flex flex-col items-center justify-center gap-10 min-h-screen'>
+      <h1>Welcome to your dashboard</h1>
+    </main>
+  )
+}
+
+export default DashboardPage;
