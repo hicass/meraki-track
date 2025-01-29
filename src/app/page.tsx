7a93612280494550b6f6cc13d26655c2750caa0b
hicass <hicass.dev@gmail.com>
2025-01-29 14:13:15 -0800

    Add functionality for a user to log in and out on the page, as well as text confirming they are logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+'use client';
+import LoginForm from "@/components/LoginForm";
+import LogoutButton from "@/components/LogoutButton";
 import SignUpForm from "@/components/SignUpForm";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Home() {
+  const { session } = useAuth();
+
   return (
     <main>
     <h1>Hello World!</h1>
 
+    {session ? (
+        <p>Hello {session.user?.user_metadata.name}!</p>
+      ) : (
+        <p>You are not logged in.</p>
+      )}
+
     <SignUpForm />
+    <LoginForm />
+    <LogoutButton />
     </main>
   );
 }
