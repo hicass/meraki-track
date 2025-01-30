be46d3b67f14b984c4e229ff6e9ca9dca08a4db5
hicass <hicass.dev@gmail.com>
2025-01-30 11:23:01 -0800

    Add styling to the home and dashboard pages, as well as links to the login page if there is no user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,34 @@
-import { FC } from "react";
+'use client';
+
+import { FC } from 'react';
+import Link from 'next/link';
+
+import { useAuth } from '@/context/AuthContext';
+import LogoutButton from '@/features/auth/LogoutButton';
 
 const DashboardPage: FC = () => {
+  const { session } = useAuth();
+
   return (
-    <main className='border flex flex-col items-center justify-center gap-10 min-h-screen'>
-      <h1>Welcome to your dashboard</h1>
+    <main className="flex flex-col items-center justify-center gap-10 min-h-screen">
+      <h1 className="text-4xl font-bold">Welcome to your dashboard</h1>
+
+      {session ? (
+        <section>
+          <p>Hello {session.user?.user_metadata.name}!</p>
+          <LogoutButton />
+        </section>
+      ) : (
+        <section className="flex items-center flex-col gap-4">
+          <p>You are not logged in.</p>
+
+          <Link href="/auth/login" className="hover:underline">
+            Login Here
+          </Link>
+        </section>
+      )}
     </main>
-  )
-}
+  );
+};
 
 export default DashboardPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client';
-import { FC } from 'react';
 
-import LogoutButton from '@/features/auth/LogoutButton';
+import { FC } from 'react';
+import Link from 'next/link';
+
 import { useAuth } from '@/context/AuthContext';
+import LogoutButton from '@/features/auth/LogoutButton';
 
 const HomePage: FC = () => {
   const { session } = useAuth();
 
   return (
     <main className="flex flex-col items-center justify-center gap-10 min-h-screen">
-      <h1>Home Page</h1>
+      <h1 className="text-4xl font-bold">Home Page</h1>
 
       {session ? (
         <section>
@@ -17,7 +19,13 @@ const HomePage: FC = () => {
           <LogoutButton />
         </section>
       ) : (
-        <p>You are not logged in.</p>
+        <section className="flex items-center flex-col gap-4">
+          <p>You are not logged in.</p>
+
+          <Link href="/auth/login" className="hover:underline">
+            Login Here
+          </Link>
+        </section>
       )}
     </main>
   );
