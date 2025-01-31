4d7b853a5aca3e91da2024b13e8c8fd77830a176
hicass <hicass.dev@gmail.com>
2025-01-31 11:28:34 -0800

    Add a simple profile page that the user can use to logout

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { FC, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { useAuth } from '@/context/AuthContext';
+import LogoutButton from '@/features/auth/LogoutButton';
+import { handleLogoutSuccess } from '@/utils/auth/auth';
+
+const DashboardProfilePage: FC = () => {
+  const { session } = useAuth();
+  const router = useRouter();
+  const userName = session?.user?.user_metadata.name;
+
+  // If there is no session, redirect to the login page
+  useEffect(() => {
+    if (!session) {
+      router.push('/auth/login');
+    }
+  }, [session]);
+
+  return (
+    <section className="border flex flex-col items-center justify-center gap-10 min-h-screen">
+      <h1 className="text-4xl font-bold">Profile Page</h1>
+
+      <section>
+        <p>Hello {userName}</p>
+        <LogoutButton onLogoutSuccess={() => handleLogoutSuccess(router)} />
+      </section>
+    </section>
+  );
+};
+
+export default DashboardProfilePage;
