5815b5392589719dbaa0da9e34e04c16319be1c6
hicass <hicass.dev@gmail.com>
2025-01-31 11:38:57 -0800

    Remove the code to log out a user from the dashboard page as it can now be accessed in the profile page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,40 +1,17 @@
 'use client';
 
 import { FC } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-
 import { useAuth } from '@/context/AuthContext';
-import LogoutButton from '@/features/auth/LogoutButton';
 
 const DashboardPage: FC = () => {
   const { session } = useAuth();
-  const router = useRouter();
   const userName = session?.user?.user_metadata.name;
-
-  const handleLogoutSuccess = () => {
-    // After successful logout, navigate to the homepage
-    router.push('/');
-  };
 
   return (
     <section className="border flex flex-col items-center justify-center gap-10 min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome to your dashboard</h1>
-
-      {session ? (
-        <section>
-          <p>Hello {userName}!</p>
-          <LogoutButton onLogoutSuccess={handleLogoutSuccess} />
-        </section>
-      ) : (
-        <section className="flex items-center flex-col gap-4">
-          <p>You are not logged in.</p>
-
-          <Link href="/auth/login" className="hover:underline">
-            Login Here
-          </Link>
-        </section>
-      )}
+      <h1 className="text-4xl font-bold">
+        Welcome to your dashboard {userName}
+      </h1>
     </section>
   );
 };
