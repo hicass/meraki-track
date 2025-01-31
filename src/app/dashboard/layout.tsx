1de6be2b18cfc8653e02500347ba17c9a93e0afe
hicass <hicass.dev@gmail.com>
2025-01-31 11:41:52 -0800

    Have the layout return a spinner instead of null while there is no session

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,8 @@
-import { FC, ReactNode } from 'react';
+'use client';
+import { FC, ReactNode, useEffect } from 'react';
+
+import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
 
 import DashboardNav from '@/features/dashboard/DashboardNav';
 
@@ -7,6 +11,19 @@ interface DashboardLayoutProps {
 }
 
 const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
+  const { session } = useAuth();
+  const router = useRouter();
+
+  // If there is no session, redirect to the login page
+  useEffect(() => {
+    if (!session) {
+      router.push('/auth/login');
+    }
+  }, [session]);
+
+  // TODO: Replace with loading spinner
+  if (!session) return null;
+
   return (
     <div className="flex">
       <DashboardNav />
