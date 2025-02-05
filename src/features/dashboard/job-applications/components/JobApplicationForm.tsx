2b4ad0528445fea08a32d8b87baedb3bcc2f858f
hicass <hicass.dev@gmail.com>
2025-02-05 13:40:51 -0500

    Add a JobApplicationForm component that renders a simple form for a user to use to submit a job app that they are tracking. Currently the user can only submit the company's name

---
+++ src/features/dashboard/job-applications/components/JobApplicationForm.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+
+import { FormSubmitHandler, OnChangeHandler } from '@/types/handlers';
+import { JobApplication } from '@/types/jobApplication';
+
+interface JobApplicationFormProps extends JobApplication {
+  handleFormChange: OnChangeHandler;
+  handleFormSubmit: FormSubmitHandler;
+}
+
+const JobApplicationForm: FC<JobApplicationFormProps> = ({
+  handleFormChange,
+  handleFormSubmit,
+  companyName,
+}) => {
+  return (
+    <form
+      className="flex flex-col gap-2 p-2 bg-white text-black"
+      onSubmit={handleFormSubmit}
+    >
+      <label>Company Name:</label>
+      <input
+        type="text"
+        name="companyName"
+        value={companyName}
+        onChange={handleFormChange}
+        required
+      />
+      <button className="bg-gray-200 p-2 hover:bg-gray-300" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default JobApplicationForm;
