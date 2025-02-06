3389328cb6c18ad3f2b48ea2aa0b59b393eb882d
hicass <hicass.dev@gmail.com>
2025-02-06 15:08:10 -0500

    Format code for type files

--- src/types/handlers.ts
+++ src/types/handlers.ts
@@ -1,2 +1,4 @@
-export type OnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => void;
+export type OnChangeHandler = (
+  event: React.ChangeEvent<HTMLInputElement>
+) => void;
 export type FormSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => void;
