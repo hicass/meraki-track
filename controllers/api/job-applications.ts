0135a2584da4f0c6a4f49771175046455af55e08
hicass <hicass.dev@gmail.com>
2025-02-06 14:55:49 -0500

    Update the job-applications controller for adding a job application to use the new jobApplicationsService to create a job application in the database

--- controllers/api/job-applications.ts
+++ controllers/api/job-applications.ts
@@ -1,3 +1,16 @@
-export async function addJobApp(req: any, res: any) {
+import { Request, Response } from 'express';
+import { createJobApp } from '../../services/jobApplicationService';
+
+export async function addJobApp(req: Request, res: Response) {
   console.log('addJobApp controller called!');
+  const body = req.body;
+  console.log('ADD JOB APP REQ BODY', body);
+
+  try {
+    const newJobApp = await createJobApp(body);
+    res.status(201).json(newJobApp);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 }
