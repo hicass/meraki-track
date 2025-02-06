dad98524562ef0930ac26761b0dae5e88e4450bc
hicass <hicass.dev@gmail.com>
2025-02-06 15:01:59 -0500

    Remove unneeded console.logs

--- controllers/api/job-applications.ts
+++ controllers/api/job-applications.ts
@@ -2,9 +2,7 @@ import { Request, Response } from 'express';
 import { createJobApp } from '../../services/jobApplicationService';
 
 export async function addJobApp(req: Request, res: Response) {
-  console.log('addJobApp controller called!');
   const body = req.body;
-  console.log('ADD JOB APP REQ BODY', body);
 
   try {
     const newJobApp = await createJobApp(body);
--- src/features/dashboard/job-applications/api/create-job-application.ts
+++ src/features/dashboard/job-applications/api/create-job-application.ts
@@ -4,6 +4,5 @@ import sendRequest from '@/utils/api/send-request';
 const BASE_URL = 'api/job-applications';
 
 export function createJobApp(newJobAppInfo: JobApplication) {
-  console.log('REQUEST CONTENTS', newJobAppInfo);
   return sendRequest(`${BASE_URL}/new`, 'POST', newJobAppInfo);
 }
