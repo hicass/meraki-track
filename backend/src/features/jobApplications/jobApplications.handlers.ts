f929f1ebdb000b7cac3bff47cab45ef406f1be6d
hicass <hicass.dev@gmail.com>
2025-02-12 13:19:56 -0800

    Remove console.log and format code for the backend jobApplicaiton handler

--- backend/src/features/jobApplications/jobApplications.handlers.ts
+++ backend/src/features/jobApplications/jobApplications.handlers.ts
@@ -2,13 +2,11 @@ import { Request, Response } from 'express';
 import { createJobApplication } from './jobApplications.service';
 
 export async function handleCreateJobApplication(req: Request, res: Response) {
-  const body = req.body;
-
-  console.log(body)
+  const newJobApplicationInfo = req.body;
 
   try {
-    const newJobApp = await createJobApplication(body);
-    res.status(201).json(newJobApp);
+    const newJobApplication = await createJobApplication(newJobApplicationInfo);
+    res.status(201).json(newJobApplication);
   } catch (error) {
     console.error('controller error: ', error);
     res.status(500).json({ error: 'Something went wrong' });
