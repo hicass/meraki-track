9b89e6bad0acf7d9783d23576ca46723fbe3eec6
hicass <hicass.dev@gmail.com>
2025-02-14 13:49:04 -0800

    Update the frontend types to have separate NewJobApplication and JobApplication types, the JobApplication type extending the NewJobApplication type with an id

--- frontend/src/features/dashboard/job-applications/api/create-job-application.ts
+++ frontend/src/features/dashboard/job-applications/api/create-job-application.ts
@@ -1,8 +1,8 @@
 import sendRequest from "@/utils/api/send-request";
-import { JobApplication } from "types/jobApplication";
+import { NewJobApplication } from "types/jobApplication";
 
 const BASE_URL = 'api/job-applications';
 
-export function createJobApplication(newJobApplicationInfo: JobApplication) {
+export function createJobApplication(newJobApplicationInfo: NewJobApplication) {
   return sendRequest(`${BASE_URL}/new`, 'POST', newJobApplicationInfo);
 }
