c74a114442389be229f7d8d1099fa59ab1d374b7
hicass <hicass.dev@gmail.com>
2025-02-17 17:33:18 -0800

    Add a getJobApplications service that queries prisma for all of a users jobApplications

--- backend/src/features/jobApplications/jobApplications.service.ts
+++ backend/src/features/jobApplications/jobApplications.service.ts
@@ -1,12 +1,21 @@
 import prisma from '@/src/config/prisma/prismaClient';
-import { NewJobApplication } from '../../types/jobApplication';
+import { JobApplication, NewJobApplication } from '../../types/jobApplication';
 
-export const createJobApplication = async ({ companyName }: NewJobApplication) => {
+export const getJobApplications = async () => {
+  const jobApplications: JobApplication[] =
+    await prisma.jobApplication.findMany();
+
+  return jobApplications;
+};
+
+export const createJobApplication = async ({
+  companyName,
+}: NewJobApplication) => {
   const job = await prisma.jobApplication.create({
     data: {
       companyName,
     },
   });
-  
+
   return job;
 };
