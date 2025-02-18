a6fa2fdf07005867e49115df13d44900aaa9f053
hicass <hicass.dev@gmail.com>
2025-02-17 17:29:22 -0800

    Add an express route for api/job-applications/new that uses the handleCreateJobApplication handler

--- backend/src/features/jobApplications/jobApplications.routes.ts
+++ backend/src/features/jobApplications/jobApplications.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { handleCreateJobApplication } from './jobApplications.handlers';
+import { handleCreateJobApplication, handleGetJobApplications } from './jobApplications.handlers';
 
 const router = express.Router();
 
+router.get('/', handleGetJobApplications)
 router.post('/new', handleCreateJobApplication);
 
 export default router;
