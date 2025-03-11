6b0e02d3664446dc84b626fac2afbd932a0ea9e1
hicass <hicass.dev@gmail.com>
2025-03-11 15:24:59 -0700

    Add a buttons to the applicationElements in the JobApplicationListTable component that can be used to delete that job application

--- frontend/src/features/dashboard/job-applications/components/JobApplicationListTable.tsx
+++ frontend/src/features/dashboard/job-applications/components/JobApplicationListTable.tsx
@@ -1,28 +1,42 @@
+import Button from '@/components/ui/Buttons/Button';
 import { FC } from 'react';
 import { JobApplication } from 'types/jobApplication';
 
 interface JobApplicationListTableProps {
   jobApplications: JobApplication[];
+  handleDeleteJobApplication: (id: number) => void;
 }
 
 const JobApplicationListTable: FC<JobApplicationListTableProps> = ({
   jobApplications,
+  handleDeleteJobApplication,
 }) => {
   const applicationElements = jobApplications.map((application) => (
     <tr key={application.id}>
-      <td className='border-r-white border-r-[0.5px] border-b-white border-b-[0.5px] p-2'>{application.companyName}</td>
+      <td>
+        <Button
+          content="x"
+          onClick={() => handleDeleteJobApplication(application.id)}
+        />
+      </td>
+      <td className="border-r-white border-r-[0.5px] border-b-white border-b-[0.5px] p-2">
+        {application.companyName}
+      </td>
     </tr>
   ));
 
-  if (!jobApplications.length){
-    return <p>No job applications found.</p>
-  } 
+  if (!jobApplications.length) {
+    return <p>No job applications found.</p>;
+  }
 
   return (
     <table>
       <thead>
         <tr>
-          <th className='border-r-white border-r-[0.5px] border-b-white border-b-[0.5px] p-2'>Company Name</th>
+          <th></th>
+          <th className="border-r-white border-r-[0.5px] border-b-white border-b-[0.5px] p-2">
+            Company Name
+          </th>
         </tr>
       </thead>
       <tbody>{applicationElements}</tbody>
