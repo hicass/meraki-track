60f38db313308486468380dd3aa2f57ee62ac674
hicass <hicass.dev@gmail.com>
2025-03-11 16:50:21 -0700

    Add tests for the backend 'deleteJobApplication' service

--- backend/src/features/jobApplications/jobApplications.service.test.ts
+++ backend/src/features/jobApplications/jobApplications.service.test.ts
@@ -1,5 +1,9 @@
 import { prismaMock } from '@/__mocks__/prismaMock';
-import { createJobApplication } from './jobApplications.service';
+import {
+  createJobApplication,
+  deleteJobApplication,
+} from './jobApplications.service';
+import { JobApplication } from '@/src/types/jobApplication';
 
 jest.mock('@/src/config/prisma/prismaClient', () => prismaMock);
 
@@ -15,5 +19,37 @@ describe('createJobApplication', () => {
     expect(prismaMock.jobApplication.create).toHaveBeenCalledWith({
       data: { companyName: 'Test Corp' },
     });
+  });
+});
+
+describe('deleteJobApplication', () => {
+  it('should delete a job application', async () => {
+    const mockJobApplicationDelete = jest.spyOn(
+      prismaMock.jobApplication,
+      'delete'
+    );
+
+    await deleteJobApplication(1);
+
+    expect(mockJobApplicationDelete.mock.calls.length).toBe(1);
+  });
+
+  it('should return an array of job applications', async () => {
+    const mockJobApplications: JobApplication[] = [
+      { id: 1, companyName: 'Test Corp' },
+      { id: 2, companyName: 'Test Corp' },
+      { id: 3, companyName: 'Test Corp' },
+      { id: 4, companyName: 'Test Corp' },
+    ];
+
+    prismaMock.jobApplication.delete.mockResolvedValue({
+      id: 5,
+      companyName: 'Test Corp',
+    });
+    prismaMock.jobApplication.findMany.mockResolvedValue(mockJobApplications);
+
+    const result = await deleteJobApplication(1);
+
+    expect(result).toBe(mockJobApplications);
   });
 });
